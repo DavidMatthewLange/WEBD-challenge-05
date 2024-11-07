26775583cc3c6e22238eb154c5fa4ba58fc7a827
David Matthew Lange <david.m.lange@outlook.com>
2024-11-07 00:14:31 -0600

    Added 676 Products using the Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Product.destroy_all
+# Category.destroy_all
+
+676.times do
+  Product.create(
+    title: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 1..666.66, as_string: false),
+    stock_quantity: Faker::Number.between(from: 1, to: 50)
+  )
+end
